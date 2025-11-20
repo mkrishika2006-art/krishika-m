8f298906ee8988e3517d835a04de83abe0803a4d
mkrishika2006-art <mkrishika2006@gmail.com>
2025-11-20 22:33:59 +0530

    Delete face_utils.py

--- face_utils.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def generate_fake_embedding():
-    return ",".join(str(random.random()) for _ in range(128))
-
-def compare_embeddings(e1, e2):
-    return random.choice([True, False])
